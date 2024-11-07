f589b689c2e369233d29e1d169d166c6d164dc47
GedSargent <120935232+GedSargent@users.noreply.github.com>
2024-11-07 11:09:35 +0000

    Add comment plan for form logic

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-// javascript
-
-console.log("Hello from index.js!");
+// Display a form to the user
+// Include input fields for the user's email address, level, and hours of study
+// Capture user's input on form submission
+// Get the user's email address - userEmail (string)
+// Get the user's level - userLevel (string)
+// Get the user's hours of study - userHours (number)
+// Validate the user's input
+// Check if the user has selected a level - check a level has been entered; check against the allowed levels; conditional statement
+// Check if the user has provided an email address - have they entered an email?; is this valid?; does it have an '@' symbol?;
+// Check if the user has specified at least one hour of study - must be equal to or greater than 1;
+// Check if the number of hours requested is within the allowed range - less than or equal to the maximum number for the level
+// Calculate the total cost
+// Calculate the total cost
+// Display the total cost to the user
